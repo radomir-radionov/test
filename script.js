736d5f8e3238931040301b9f6f845e8540e81005
radomir <radomir.radionov26@gmail.com>
2024-07-24 12:14:13 +0300

    tttt

---
+++ script.js
@@ -0,0 +1,53 @@
+const notifications = document.querySelector(".notifications"),
+  buttons = document.querySelectorAll(".buttons .btn");
+
+const btnSignInEl = document.querySelector("#btn-sign-in");
+
+const toastDetails = {
+  timer: 5000,
+  success: {
+    icon: "fa-circle-check",
+    text: "Hello World: This is a success toast.",
+  },
+  error: {
+    icon: "fa-circle-xmark",
+    text: "Hello World: This is an error toast.",
+  },
+  warning: {
+    icon: "fa-triangle-exclamation",
+    text: "Hello World: This is a warning toast.",
+  },
+  info: {
+    icon: "fa-circle-info",
+    text: "Hello World: This is an information toast.",
+  },
+  random: {
+    icon: "fa-solid fa-star",
+    text: "Hello World: This is a random toast.",
+  },
+};
+
+const removeToast = (toast) => {
+  toast.classList.add("hide");
+  if (toast.timeoutId) clearTimeout(toast.timeoutId);
+  setTimeout(() => toast.remove(), 500);
+};
+
+const createToast = (id) => {
+  const { icon, text } = toastDetails[id];
+  const toast = document.createElement("li");
+  toast.className = `toast ${id}`;
+  toast.innerHTML = `<div class="column">
+                         <i class="fa-solid ${icon}"></i>
+                         <span>${text}</span>
+                      </div>
+                      <i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>`;
+  notifications.appendChild(toast);
+  toast.timeoutId = setTimeout(() => removeToast(toast), toastDetails.timer);
+};
+
+const handlebtnSignInElClick = () => {
+  createToast("success");
+};
+
+btnSignInEl.addEventListener("click", handlebtnSignInElClick);
